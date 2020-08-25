019ae00eec128b2ab836e7c85d705bc0cc0ce531
kmj <hi.minjungkim@gmail.com>
2020-08-25 12:11:17 +0900

    구조 변경

--- httpClient.js
+++ httpClient.js
@@ -2,24 +2,24 @@ import http from 'http';
 
 // 호출할 페이지 정보 설정
 const options = {
-  host: "127.0.0.1",
-  port: 8000,
-  path: "/"
-}
+  host: '127.0.0.1',
+  port: 80,
+  path: '/',
+};
 
 // 페이지 호출
 const req = http.request(options, (res) => {
-  let data = "";
+  let data = '';
 
   // 서버에서 받아온 데이터 수신
   res.on('data', (chunk) => {
     data += chunk;
-  })
+  });
 
   // 수신 완료하면 console 출력
   res.on('end', () => {
     console.log(data);
-  })
-})
+  });
+});
 
 req.end();
--- httpServer.js
+++ httpServer.js
@@ -1,12 +1,24 @@
 import http from 'http';
 
 // HTTP 서버 인스턴스 생성
-const server = http.createServer( (req, res) => {
-  res.writeHead(200, { 
-    'Content-Type': 'text/plain'
-  });
-  res.end('hello world');
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html', host: 'kmj' });
+    res.write(
+      '<html><body><p>Homepage</p><img src="https://kdydesign.github.io/2017/07/15/nodejs-npm-tutorial/cover.png"/></body></html>'
+    );
+    res.end(console.log('end of homepage'));
+  } else if (req.url == '/admin') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>Admin page</p></body></html>');
+  } else if (req.url == '/data') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(JSON.stringify({ message: 'Hello World' }));
+    res.end();
+  } else {
+    res.end('Invalid Request');
+  }
 });
 
 // 서버에 포트 할당
-server.listen(8000);
+server.listen(80);
